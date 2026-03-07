7a85acabbefc635b2404b77dc5cdaf33c39099bf
Willy Hetland <willeyh@gmail.com>
2026-03-07 22:29:09 +0100

    tailwind and nuxt config

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import 'tailwindcss/tailwind.css'
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center">
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Chat />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
+<script setup lang="ts">
+</script>
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
     <Chat />
   </div>
 </template>
 
-<script setup lang="ts">
-import Chat from '@/components/Chat.vue'
-</script>
