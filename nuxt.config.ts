1267dfcb9de5790e81af173fd53e92a02c4114be
Willy Hetland <willeyh@gmail.com>
2026-03-07 22:53:19 +0100

    structure fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['./app/assets/css/tailwind.css'],
+  ssr: false,
+  css: ["./app/assets/css/tailwind.css"],
   vite: {
     plugins: [tailwindcss()],
-  }
-})
+  },
+});
