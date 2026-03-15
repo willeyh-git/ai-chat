1c31823d7ef69cd05d67ae14d669d6646e5cc155
Willy Hetland <willeyh@gmail.com>
2026-03-15 21:49:26 +0100

    remove unused pages

--- app/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Chat from "./chat.vue";
-</script>
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
-    <Chat />
-  </div>
-</template>
-
